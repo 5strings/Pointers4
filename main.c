76ed8676ee5e046db85ca53a499eccead31fd326
5strings <5strings@users.noreply.github.com>
2017-11-28 11:34:05 +1100

    Update main.c

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main() {
     int a[5],i,sum=0;
     int *p;
     
-    printf("Enter 5 elememts of the arry: \n");
+    printf("Enter 5 elememts of the array: \n");
     for (i=0; i<5; i++)
         scanf("%d",&a[i]);
     
